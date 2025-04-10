284c674b038802ea072ab5e3e925f49cc2d136bf
Gabe Galiatsatos <galiatgg@mail.uc.edu>
2025-04-10 10:42:57 -0400

    Add project files.

---
+++ galiatgg_Assignment10/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="galiatgg_Assignment10.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form runat="server">
+    <asp:Panel runat="server" ID="container">
+
+        <asp:HyperLink runat="server" ID="lnknasa">Go to NASA.gov</asp:HyperLink>
+        <br />
+        <br />
+
+        <asp:Label runat="server" ID="lblabout">About Space Exploration</asp:Label>
+        <br />
+        <asp:Literal runat="server" ID="txtabout"></asp:Literal>
+
+
+        <br />
+    <asp:Label runat="server" ID="lblkeymission">Key Missions</asp:Label>
+        <asp:BulletedList runat="server" ID="blkeymissions"></asp:BulletedList>
+
+
+
+        <asp:Label runat="server" ID="lblSpaceAgencies">Notable Space Agencies</asp:Label>
+        <asp:Table runat="server" ID="tblSpaceAgencies" BorderWidth="1" GridLines="Both"></asp:Table>
+
+        <asp:Label runat="server" ID="lblquote">Famous Quote</asp:Label>
+        <br />
+        <asp:Literal runat="server" ID="txtquote"></asp:Literal>
+        <br />
+        <asp:Label runat="server" ID="lblgallery">Gallery</asp:Label>
+        <br />
+        <asp:Image runat="server" ID="img1" />
+    </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ galiatgg_Assignment10/Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace galiatgg_Assignment10
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var tableheader = new TableRow();
+            var columnheader1 = new TableCell();
+            var columnheader2 = new TableCell();
+
+            columnheader1.Text = "Agency";
+            columnheader2.Text = "Country";
+
+            tableheader.Cells.Add(columnheader1);
+            tableheader.Cells.Add(columnheader2);
+
+            tblSpaceAgencies.Rows.Add(tableheader);
+
+            var nasarow = new TableRow();
+            var nasa1 = new TableCell();
+            var nasa2 = new TableCell();
+
+            nasa1.Text = "NASA";
+            nasa2.Text = "USA";
+
+            nasarow.Cells.Add(nasa1);
+            nasarow.Cells.Add(nasa2);
+
+            tblSpaceAgencies.Rows.Add(nasarow);
+
+            var esarow = new TableRow();
+            var esa1 = new TableCell();
+            var esa2 = new TableCell();
+
+            esa1.Text = "ESA";
+            esa2.Text = "Europe";
+
+            esarow.Cells.Add(esa1);
+            esarow.Cells.Add(esa2);
+
+            tblSpaceAgencies.Rows.Add(esarow);
+
+            var isrorow = new TableRow();
+            var isro1 = new TableCell();
+            var isro2 = new TableCell();
+
+            esa1.Text = "ISRO";
+            esa2.Text = "India";
+
+            isrorow.Cells.Add(isro1);
+            isrorow.Cells.Add(isro2);
+
+            tblSpaceAgencies.Rows.Add(isrorow);
+
+            blkeymissions.Items.Add("Apollo 11 - First Moon Landing (1969)");
+            blkeymissions.Items.Add("Voyager 1 & 2 - Deep Space Probes");
+            blkeymissions.Items.Add("Mars Rovers - Curiosity, Perseverance");
+
+            img1.ImageUrl = "space_image.jpg";
+
+            txtabout.Text = "Space exploration has been one of humanity's greatest endeavors, leading to incredible discoveries about our universe.";
+            txtquote.Text = "\"That's one small step for man, one giant leap for mankind.\" – Neil Armstrong";
+
+            lnknasa.NavigateUrl = "https://nasa.gov";
+        }
+    }
+}
